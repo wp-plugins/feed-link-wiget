ab6ff7f62bc185ec355e276b418cc06d1e816f89
dhoppe <dhoppe@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-01-01 09:57:25 +0000

    git-svn-id: https://plugins.svn.wordpress.org/feed-link-wiget/trunk@188629 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,39 @@
+=== Feed Link Wiget ===
+Contributors: dhoppe
+Tags: widget, sidebar, rss, rss2, atom, feed, badge, posts, comments  
+Requires at least: 2.8
+Tested up to: 2.9
+Stable tag: trunk
+
+This Plugin enables you to add your feed links to a widget area.
+
+== Description ==
+
+This Widget shows links (you can set this up) to your
+
+* rss2 post feed
+* rss2 comments feed
+* atom post feed
+* atom comment feed 
+
+in your Sidebar.
+You can choose if the user should see a little help link to the wiki post "What are feeds and what the hell is rss".
+
+
+* The widget is aviable in english.
+* Das Widget ist in deutsch verfügbar. (In der dt. Version zeigt der Hilfelink auf eine dt. Seite.)
+ 
+
+== Installation ==
+
+Installation as usual.
+
+1. Unzip and Upload all files to a sub directory in "/wp-content/plugins/".
+1. Activate the plugin through the 'Plugins' menu in WordPress.
+1. Go to your widget admin page and choose a place for your new Widget.
+1. You like what you see?
+
+== Changelog ==
+
+= 1.0 =
+* Everything works fine.
---
+++ wp-plugin-widget-feed-link.php
@@ -0,0 +1,106 @@
+<?php
+
+/*
+
+Plugin Name: Feed Link Wiget
+Description: Enables you to add your feed links to a widget area.
+Plugin URL: http://dennishoppe.de/wordpress-plugins/feed-link-widget
+Author: Dennis Hoppe
+Author URI: http://dennishoppe.de
+Version: 1.0
+
+*/
+
+If (!Class_Exists('wp_plugin_widget_feed_link')){
+Class wp_plugin_widget_feed_link extends WP_Widget {
+  var $base_url;
+  var $text_domain;
+
+  Function wp_plugin_widget_feed_link(){
+    // Set Widget Settings
+    $this->WP_Widget('feedlinker', 'Feed Link Widget');
+    
+    // Read base
+    $this->base_url = get_option('home').'/'.Str_Replace("\\", '/', SubStr(  RealPath(DirName(__FILE__)), Strlen(ABSPATH) ));
+    
+    // Get ready to translate
+    $this->Load_TextDomain();
+    
+    // Add hooks
+    Add_Action ('wp_head', Array($this, 'IncludeCSS'));
+    Add_Action ('widgets_init', Array ($this, 'Register'));
+  }
+  
+  Function Register(){
+    Register_Widget (get_class($this));
+  }
+  
+  Function Load_TextDomain(){
+    $this->text_domain = get_class($this);
+    load_textdomain ($this->text_domain, DirName(__FILE__).'/language/'.get_locale().'.mo');
+  }
+  
+  Function t ($text, $context = ''){
+    // Translates the string $text with context $context
+    If ($context == '')
+      return __($text, $this->text_domain);
+    Else
+      return _x($text, $context, $this->text_domain);
+  }
+
+  Function IncludeCSS(){
+    Echo '<link rel="stylesheet" type="text/css" href="'.$this->base_url.'/wp-plugin-widget-feed-link.css" />';
+  }
+  
+  Function widget($args, $settings){
+    Echo $args['before_widget'];
+    Echo $args['before_title']; Echo $settings['title']; Echo $args['after_title'] ?>
+    
+    <ul class="wp_plugin_widget_feed_link">
+      <?php If ($settings['article_rss']) : ?>
+        <li class="rss"><a href="<?php bloginfo('rss2_url'); ?>" title="<?php Echo $this->t('Subscribe posts via RSS') ?>"><?php Echo $this->t('Subscribe posts via RSS') ?></a></li>
+      <?php EndIf; ?>
+      
+      <?php If ($settings['article_atom']) : ?>
+        <li class="atom"><a href="<?php bloginfo('atom_url'); ?>" title="<?php Echo $this->t('Subscribe posts via Atom') ?>"><?php Echo $this->t('Subscribe posts via Atom') ?></a></li>
+      <?php EndIf; ?>
+      
+      <?php If ($settings['comments_rss']) : ?>
+        <li class="rss"><a href="<?php bloginfo('comments_rss2_url'); ?>" title="<?php Echo $this->t('Subscribe comments via RSS') ?>"><?php Echo $this->t('Subscribe comments via RSS') ?></a></li>
+      <?php EndIf; ?>
+      
+      <?php If ($settings['comments_atom']) : ?>
+        <li class="atom"><a href="<?php bloginfo('comments_atom_url'); ?>" title="<?php Echo $this->t('Subscribe comments via Atom') ?>"><?php Echo $this->t('Subscribe comments via Atom') ?></a></li>
+      <?php EndIf; ?>
+
+      <?php If ($settings['show_help']) : ?>
+        <li class="help"><a href="<?php Echo $this->t('http://en.wikipedia.org/wiki/RSS') ?>" title="<?php Echo $this->t('What is that?') ?>"><?php Echo $this->t('What is that?') ?></a></li>
+      <?php EndIf; ?>
+
+    </ul>
+    
+    <?php Echo $args['after_widget'];
+  }
+  
+  Function form($settings){
+    // Form to configure the Widget in Admin panel
+    Echo $this->t('Title:') ?> <input type="text" name="<?php Echo $this->get_field_name('title') ?>" value="<?php Echo $settings['title'] ?>" />
+    <h3><?php Echo $this->t('Show subscribtion links:') ?></h3>
+    <ul>
+      <li><input type="checkbox" name="<?php Echo $this->get_field_name('article_rss') ?>" <?php checked($settings['article_rss'], 'on') ?> value="on" /><?php Echo $this->t('Posts via RSS') ?></li>
+      <li><input type="checkbox" name="<?php Echo $this->get_field_name('article_atom') ?>" <?php checked($settings['article_atom'], 'on') ?> value="on" /><?php Echo $this->t('Posts via Atom') ?></li>
+      <li><input type="checkbox" name="<?php Echo $this->get_field_name('comments_rss') ?>" <?php checked($settings['comments_rss'], 'on') ?> value="on" /><?php Echo $this->t('Comments via RSS') ?></li>
+      <li><input type="checkbox" name="<?php Echo $this->get_field_name('comments_atom') ?>" <?php checked($settings['comments_atom'], 'on') ?> value="on" /><?php Echo $this->t('Comments via Atom') ?></li>
+    </ul>
+    <input type="checkbox" name="<?php Echo $this->get_field_name('show_help') ?>" <?php checked($settings['show_help'], 'on') ?> value="on"/><?php Echo $this->t('Show a help link.') ?><br />
+    <?php
+  }
+  
+  Function update($new_settings, $old_settings){
+    // Save the $new_settings Array
+    return $new_settings;
+  }
+} /* End of the Class */
+New wp_plugin_widget_feed_link();
+} /* End of the If-Class-Exists-Condition */
+/* End of File */
